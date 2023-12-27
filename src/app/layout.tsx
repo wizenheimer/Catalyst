2ec996d1c51ed62d48eea160ad674c411d6ee074
Nayan <91504165+wizenheimer@users.noreply.github.com>
2023-12-27 22:43:23 +0530

    feat: add auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/services/utils';
+import NavBar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
                     inter.className
                 )}
             >
+                <NavBar />
                 {children}
             </body>
         </html>
